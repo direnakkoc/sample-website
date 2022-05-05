29189994eb49ae21d9672d4a559f333bb2f64345
direnakkocdemir <drnakkoc94@gmail.com>
2022-05-05 10:07:59 +0100

    Extract first 500 words from all posts

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,16 @@
+const moment = require('moment')
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addWatchTarget('./src/_includes/styles/tailwind.css')
   eleventyConfig.addPassthroughCopy({ './src/_includes/static': 'static' })
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+ 
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).utc().format('LL'); // E.g. May 31, 2019
+  })
+
   return {
     dir: {
       input: "src",
